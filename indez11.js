eb6172a085f97a42578d483f81041c2c7bc88bba
LS.551 <medinajoseluis412@gmail.com>
2023-03-30 13:43:28 -0400

    new file

---
+++ indez11.js
@@ -0,0 +1,25 @@
+/*var x = 5 
+
+
+
+/*if(x > 8) {
+ console.log("la condicion es verdadera");
+}
+
+/*if(x > 2) {
+    console.log("la condicion es falsa")
+}*/
+
+var estacion = "invierno";
+
+if (estacion == "invierno") {
+    console.log("!si! me encanta el invierno");
+}
+
+if (estacion == "primavera") {
+    console.log("!si! me encanta la primavera");
+}
+
+if (estacion == "verano") {
+    console.log("!si! me encanta el verano");
+}

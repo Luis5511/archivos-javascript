7342007097c2c639fb60cc3abc6e9a73e0dcc174
LS.551 <medinajoseluis412@gmail.com>
2023-03-30 13:03:32 -0400

    new file

---
+++ indez09.js
@@ -0,0 +1,16 @@
+//console.log(true || true); --true
+//console.log(true || false); --true
+//console.log(false || true);  --true
+//console.log(false || false); --false
+
+//var a = 10;
+
+//console.log((a < 5) || (a > 7));
+
+//var b = 20;
+
+//console.log((b > 17) || (b < 10))
+
+var a =15;
+
+console.log((a == 5) || (a < 10))

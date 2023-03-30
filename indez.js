55f1d488f089cac7fe9346989004a2f0c2c4da26
LS.551 <medinajoseluis412@gmail.com>
2023-03-30 12:20:23 -0400

    upload and upload of the file

--- indez.js
+++ indez.js
@@ -10,3 +10,14 @@ console.log(b === a);
 b = 8
 
 console.log(a === b);
+
+b= "5";
+
+console.log(a == b);
+
+console.log(b === a);
+
+a = "java"
+b = "java"
+
+console.log(a === b);
---
+++ indez02.js
@@ -0,0 +1,7 @@
+//console.log( 9 != 6)
+
+//console.log(9 != 9)
+
+//console.log("javascript" != "javascript")
+
+console.log([1,2,3] != [1,2,3]);

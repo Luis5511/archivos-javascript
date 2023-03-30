904f1858752113f25ed5f66291c9a80c5b8e97ce
LS.551 <medinajoseluis412@gmail.com>
2023-03-30 12:30:52 -0400

    upload new file

---
+++ indez04.js
@@ -0,0 +1,15 @@
+//console.log(6 > 5);
+//console.log(10 < 4);
+
+//console.log("b" > "a");
+//console.log("c" > "c");
+//console.log("ACB" > "ABC");
+//console.log('Ab' > 'a');
+
+//console.log("hola" > "mundo");
+
+var a = 15;
+var b = 4;
+
+console.log(a > b);
+console.log(b > a);

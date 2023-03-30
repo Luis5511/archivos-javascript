85e0cd1fabd3bc11e51bf50a1e0224e734daa6b4
LS.551 <medinajoseluis412@gmail.com>
2023-03-30 14:36:17 -0400

    new file

---
+++ indez12.js
@@ -0,0 +1,28 @@
+/*var x = 8;
+
+
+if(x > 8) {
+ console.log("la condicion es verdadera");
+} else {
+    console.log("la condicion es falsa.");
+}*/
+
+/*var meses = "enero";
+
+if(meses == "enero"){
+    console.log("si, estamos en el mes de enero")
+} else { console.log("si,estamos en el mes de febrero")
+}*/
+
+/*function clasificarvalor(valor) {
+    if (valor % 2 == 0) {
+         console.log("divisible entre 2."); }
+else if (valor % 3 == 0){
+ console.log("divisible entre 3.");
+} else{
+    console.log("no es divisible entre las opciones.");
+}
+}
+//clasificarvalor(2);
+//clasificarvalor(15);
+clasificarvalor(5) */
